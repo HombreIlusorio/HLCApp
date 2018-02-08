fa71235d6c6d4fc04b8c8ea98a3dd5857befb0e4
HombreIlusorio <cfsamorai95@gmail.com>
2018-02-08 19:18:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/carlos/hlcapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.carlos.hlcapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static String EXTRA_MESSAGE;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+}
+
+ 
